d98a532db513b128bd210d7b3d0054119afda28a
srijans5257 <srijans5257@gmail.com>
2024-09-28 22:45:11 +0530

    Delete Case-Study.. directory

--- Case-Study../wfh_project/employee_management/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Employee, WFHApplication, WFHTask
-
-admin.site.register(Project)
-admin.site.register(Employee)
-admin.site.register(WFHApplication)
-admin.site.register(WFHTask)
--- Case-Study../wfh_project/employee_management/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmployeeManagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'employee_management'
